9e728a756af52eb8b560c3e9961bbfb4badd3595
Alec Resnick <aresnickety@gmail.com>
2020-09-25 16:05:12 -0400

    Add mathjax MML parsing and enriching of calculator state objects

--- get-desmos-mathml/index.js
+++ get-desmos-mathml/index.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
+const mathjax = require("mathjax-node");
+mathjax.config({
+    MathJax: {
+        // default
+    }
+});
+mathjax.start();
+
 
 /* Check that we receive exactly one command line argument,
 and that it is a ten character, lower case hash */
@@ -10,21 +18,25 @@ const validHash =
 
 console.assert(
     validHash,
-    "Expected a ten character, lowercase hash, received " + process.argv.slice(2).join(" ")
+    "Expected a ten character, lowercase hash, received",
+    process.argv.slice(2).join(" ")
 );
 
 // If we get a valid hash, assign it
 if (validHash) {
     const hash = process.argv[2];;
 
-    // Observed by inspecting network traffic on desmos.com/calculator when clicking Save
+    // Observed by inspecting network traffic 
+    // on desmos.com/calculator when clicking Save
     const endpoints = {
         state: "https://saved-work.desmos.com/calc-states/production"
     };
 
     // Check our endpoints do not have trailing slashes
     const validEndpoints = Object.values(endpoints).every(v => v[v.length - 1] !== "/")
-    console.assert(validEndpoints, "Endpoints cannot have trailing slashes, ", endpoints);
+    console.assert(
+        validEndpoints, 
+        "Endpoints cannot have trailing slashes,", endpoints);
 
     // Construct and GET the state url
     async function getCalcState(hash) {
@@ -34,6 +46,33 @@ if (validHash) {
         return data;
     }
 
+    async function addMathMLToCalcState(calcState) {
+        // mathjax returns a promise; so we return a promise which 
+        // will resolve when all mathjax promises resolve
+        return Promise.all(
+            // For all the expressions in the list
+            calcState.expressions.list.map(async function(expression) {
+                // Check if it's an expression, as opposed to text, etc.
+                if (expression.type == "expression") {
+                    // and then if it is, typeset as MathML 
+                    // and add the result to a new key in the expression object
+                    expression.mathml = await mathjax.typeset({
+                        math: expression.latex,
+                        format: "TeX",
+                        mml: true
+                    });
+                }
+
+                // return the object (non-expressions unchangd)
+                return expression; 
+            })
+        );
+    }
+
+    // Get the state
     getCalcState(hash)
-        .then(data => console.log(data));
+        .then(function(state) {
+            addMathMLToCalcState(state)
+            .then(mmledState => console.log(mmledState));
+        });
 }
