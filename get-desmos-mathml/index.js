331800c88bd2be66105de5d8e7d0faf930c94baa
Alec Resnick <aresnickety@gmail.com>
2020-09-25 15:27:28 -0400

    Add check for command line arguments as 10 letter lowercase hash

--- get-desmos-mathml/index.js
+++ get-desmos-mathml/index.js
@@ -1,1 +1,7 @@
 #!/usr/bin/env node
+
+console.assert(
+    process.argv.length == 3 &&
+    process.argv[2].match(/^[a-z0-9]{10}$/) !== null,
+    "Expected a ten character, lowercase hash, received " + process.argv.slice(2).join(" ")
+    )
