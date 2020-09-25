ac606cb38174a3c7b64905031506ddb9a29d7b2f
Alec Resnick <aresnickety@gmail.com>
2020-09-25 15:36:31 -0400

    Add comments, const where appropriate

--- get-desmos-mathml/index.js
+++ get-desmos-mathml/index.js
@@ -2,6 +2,8 @@
 
 const fetch = require('node-fetch');
 
+/* Check that we receive exactly one command line argument,
+and that it is a ten character, lower case hash */
 const validHash =
     process.argv.length == 3 &&
     process.argv[2].match(/^[a-z0-9]{10}$/) !== null;
@@ -11,20 +13,24 @@ console.assert(
     "Expected a ten character, lowercase hash, received " + process.argv.slice(2).join(" ")
 );
 
+// If we get a valid hash, assign it
 if (validHash) {
     const hash = process.argv[2];;
 
+    // Observed by inspecting network traffic on desmos.com/calculator when clicking Save
     const endpoints = {
         state: "https://saved-work.desmos.com/calc-states/production"
     };
 
+    // Check our endpoints do not have trailing slashes
     const validEndpoints = Object.values(endpoints).every(v => v[v.length - 1] !== "/")
     console.assert(validEndpoints, "Endpoints cannot have trailing slashes, ", endpoints);
 
+    // Construct and GET the state url
     async function getCalcState(hash) {
-        let stateURL = [endpoints.state, hash].join('/')
-        let response = await fetch(stateURL);
-        let data = await response.json()
+        const stateURL = [endpoints.state, hash].join('/')
+        const response = await fetch(stateURL);
+        const data = await response.json()
         return data;
     }
 
