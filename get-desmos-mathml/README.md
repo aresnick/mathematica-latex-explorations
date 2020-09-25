ee8a992ba4ab57a95773f2e055a8300e401e87cf
Alec Resnick <aresnickety@gmail.com>
2020-09-25 16:31:05 -0400

    First draft of README and media, with background

--- get-desmos-mathml/README.md
+++ get-desmos-mathml/README.md
@@ -1,1 +1,35 @@
 # README
+
+This is a small command-line node script which, provided the id from a Desmos share link, returns a JSON object of the collection of expressions, with the mathematical expressions enriched by parsing Desmos's internal LaTeX representation into MathML.  It was created as a demonstration to support explorations of how to parse mathematical work as part of the [PBLL](https://www.notion.so/An-introduction-to-the-PBLL-624f34255da844228dc1276221d791c9).
+
+---
+
+## Background
+
+As far as I know, this is an undocumented API for Desmos.  I found it by observing network behavior when you save a calculator in Desmos.
+
+When you save a blank calculator…
+![Screenshot of empty Desmos calculator and the developer console](/readme/empty-save.png)
+
+When you save a calculator with `x^2`…
+![Screenshot of Desmos calculator with a single x^2 and the developer console](/readme/x2-save.png)
+
+When you look at the GET request to the `stateURL`
+![Screenshot of the GET request to the stateURL and the developer console](/readme/calc-state-get.png)
+
+When you save a calculator with multiple expressions…
+![Screenshot of Desmos calculator with multiple expressions and the developer console](/readme/multi-expression-save.png)
+
+When you look at the prettified JSON for the `stateURL` for that multiple expression notebook…
+![Screenshot of prettified JSON response from the state URL for a multiple expression notebook and the developer console](/readme/multi-expression-save.png)
+
+This is a very simple setup, and we use [`node-fetch`](https://github.com/node-fetch/node-fetch) to fetch and parse the JSON and [`mathjax-node`](https://github.com/mathjax/MathJax-node) to parse the LaTeX into [MathML](https://www.w3.org/Math/whatIsMathML.html).
+
+## To use
+
+1. Find or create a Desmos project.
+2. "Share" the Desmos project by clicking the share button like this
+    ![Image of share button in Desmos](/readme/share-link.png)
+3. Copy the link indicated, and extract the ten character ID, _e.g_ `yahb3wbmu0` from `https://www.desmos.com/calculator/yahb3wbmu0`)
+4. `git clone` this repository and run `npm install` within the resulting folder.
+5. Run `node mml-parse.js $ID`, where `$ID` is replaced with the ID you found in step 3.
---
+++ get-desmos-mathml/readme/mml-parse.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+
+const fetch = require("node-fetch");
+const mathjax = require("mathjax-node");
+mathjax.config({
+    MathJax: {
+        // use the default MathJax configuration
+    }
+});
+mathjax.start();  // Loads the configuration, per https://github.com/mathjax/MathJax-node#start
+
+
+/* Check that we receive exactly one command line argument,
+and that it is a ten character, lower case hash */
+const validHash =
+    process.argv.length == 3 &&
+    process.argv[2].match(/^[a-z0-9]{10}$/) !== null;
+
+console.assert(
+    validId,
+    "Expected a ten character, lowercase id, received",
+    process.argv.slice(2).join(" ")
+);
+
+// If we get a valid hash, assign it
+if (validId) {
+    const id = process.argv[2];;
+
+    // Observed by inspecting network traffic 
+    // on desmos.com/calculator when clicking Save
+    const endpoints = {
+        state: "https://saved-work.desmos.com/calc-states/production"
+    };
+
+    // Check our endpoints do not have trailing slashes
+    const validEndpoints = Object.values(endpoints).every(v => v[v.length - 1] !== "/")
+    console.assert(
+        validEndpoints, 
+        "Endpoints cannot have trailing slashes,", endpoints);
+
+    // Construct and GET the state url
+    async function getCalcState(id) {
+        const stateURL = [endpoints.state, id].join('/')
+        const response = await fetch(stateURL);
+        const data = await response.json()
+        return data;
+    }
+
+    async function addMathMLToCalcState(calcState) {
+        // mathjax returns a promise; so we return a promise which 
+        // will resolve when all mathjax promises resolve
+        return Promise.all(
+            // For all the expressions in the list
+            calcState.expressions.list.map(async function(expression) {
+                // Check if it's an expression, as opposed to text, etc.
+                if (expression.type == "expression") {
+                    // and then if it is, typeset as MathML 
+                    // and add the result to a new key in the expression object
+                    expression.mathml = await mathjax.typeset({
+                        math: expression.latex,
+                        format: "TeX",
+                        mml: true
+                    });
+                }
+
+                // return the object (non-expressions unchangd)
+                return expression; 
+            })
+        );
+    }
+
+    // Get the state
+    getCalcState(id)
+        .then(function(state) {
+            // Enrich with MathML
+            addMathMLToCalcState(state)
+            .then(mmledState => console.log(mmledState));
+        });
+}
