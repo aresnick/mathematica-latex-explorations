74e7ec5b6a9d3225849399f22d870163028e103f
Alec Resnick <aresnickety@gmail.com>
2020-09-25 15:34:29 -0400

    Basic fetch of calc state from endpoints sniffed from network traffic

--- get-desmos-mathml/index.js
+++ get-desmos-mathml/index.js
@@ -1,7 +1,33 @@
 #!/usr/bin/env node
 
-console.assert(
+const fetch = require('node-fetch');
+
+const validHash =
     process.argv.length == 3 &&
-    process.argv[2].match(/^[a-z0-9]{10}$/) !== null,
+    process.argv[2].match(/^[a-z0-9]{10}$/) !== null;
+
+console.assert(
+    validHash,
     "Expected a ten character, lowercase hash, received " + process.argv.slice(2).join(" ")
-    )
+);
+
+if (validHash) {
+    const hash = process.argv[2];;
+
+    const endpoints = {
+        state: "https://saved-work.desmos.com/calc-states/production"
+    };
+
+    const validEndpoints = Object.values(endpoints).every(v => v[v.length - 1] !== "/")
+    console.assert(validEndpoints, "Endpoints cannot have trailing slashes, ", endpoints);
+
+    async function getCalcState(hash) {
+        let stateURL = [endpoints.state, hash].join('/')
+        let response = await fetch(stateURL);
+        let data = await response.json()
+        return data;
+    }
+
+    getCalcState(hash)
+        .then(data => console.log(data));
+}
